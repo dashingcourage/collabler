513069960c00d9d7511730336dd511d17288d480
Yuki S <dashingcourage@outlook.com>
2020-01-26 21:14:48 +0900

    Creating a user edit page #3

---
+++ db/migrate/20200126045636_add_personalinfo_to_user.rb
@@ -0,0 +1,9 @@
+class AddPersonalinfoToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :website, :string
+    add_column :users, :self_intro, :text
+    add_column :users, :phone_number, :string
+    add_column :users, :sex, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_142658) do
+ActiveRecord::Schema.define(version: 2020_01_26_054557) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2020_01_25_142658) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "username"
+    t.string "website"
+    t.text "self_intro"
+    t.string "phone_number"
+    t.string "sex"
+    t.string "profile_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
