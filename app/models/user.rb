e9f26fb15ecddc7f85d6d571c6ff77307f2563fd
Yuki S <dashingcourage@outlook.com>
2020-01-27 23:21:47 +0900

    Im still working on it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes
   has_many :comments
+  has_many :active_relationships, class_name:  "Relationship",foreign_key: "follower_id",dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name:  "Relationship", foreign_key: "followed_id", dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -19,6 +24,18 @@ class User < ApplicationRecord
     result = update_attributes(params, *options)
     clean_up_passwords
     result
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   root 'posts#index'
   get '/users/:id', to: 'users#show', as: 'user'
 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
     resources :likes, only: %i(create destroy)
     resources :comments, only: %i(create destroy)
   end
+  resources :relationships, only: %i(create destroy)
 end
