a92022032a18d35f0f084dfa762139182565349b
Yuki S <dashingcourage@outlook.com>
2020-01-27 17:46:20 +0900

    Additional features ”Likes and Comments" #7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
+    @posts = @user.posts.includes(:photos, :likes, :comments)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+
+  def full_title(page_title='')
+    base_title = "Collabler"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
   def avatar_url(user)
     return user.profile_photo unless user.profile_photo.nil?
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
