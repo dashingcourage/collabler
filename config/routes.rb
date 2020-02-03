a4afd83c6ba1dc07d503e2d2e2a862bb2d551f36
Yuki S <dashingcourage@outlook.com>
2020-02-03 22:37:41 +0900

    Adding password change page #13

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,19 @@ Rails.application.routes.draw do
     end
   end
 
+  resource :user, only: %i(edit) do
+    collection do
+      patch 'update_password'
+    end
+  end
+
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
     resources :likes, only: %i(create destroy)
     resources :comments, only: %i(create destroy)
     resources :bookmarks, only: %i(create destroy)
   end
+  
   resources :relationships, only: %i(create destroy)
   resources :notifications, only: :index
 end
